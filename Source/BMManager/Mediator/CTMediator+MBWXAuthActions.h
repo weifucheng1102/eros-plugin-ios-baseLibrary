2f3b717ccfbf7e14bd62b1269ff60930f74e9c39
dmlzj <284832506@qq.com>
2018-12-30 11:25:34 +0800

    ok

---
+++ Source/BMManager/Mediator/CTMediator+MBWXAuthActions.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+MBWXAuthActions.h
+//  AFNetworking
+//
+//  Created by mac3 on 2018/12/30.
+//
+
+#import "CTMediator.h"
+
+@interface CTMediator (MBWXAuthActions)
+- (BOOL)CTMediator_WXAuthHandleOpenURL:(NSDictionary *)info;
+
+@end
---
+++ Source/BMManager/Mediator/CTMediator+MBWXAuthActions.m
@@ -0,0 +1,21 @@
+//
+//  CTMediator+MBWXAuthActions.m
+//  AFNetworking
+//
+//  Created by mac3 on 2018/12/30.
+//
+
+#import "CTMediator+MBWXAuthActions.h"
+NSString *const kCTMediatorWXAuthTarget = @"BMWXAuth";
+NSString *const kCTMediatorActionWXAuthHandleOpenURL = @"AuthHandleOpenURL";
+@implementation CTMediator (MBWXAuthActions)
+- (BOOL)CTMediator_WXAuthHandleOpenURL:(NSDictionary *)info
+{
+    id result = [self performTarget:kCTMediatorWXAuthTarget
+                             action:kCTMediatorActionWXAuthHandleOpenURL
+                             params:info
+                  shouldCacheTarget:NO];
+    return [result boolValue];
+}
+
+@end
