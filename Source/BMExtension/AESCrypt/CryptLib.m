78d33a439b75509981ec036760f784d0a67cd1a1
dmlzj <284832506@qq.com>
2020-03-19 15:06:17 +0800

    重复的函数

--- Source/BMExtension/AESCrypt/CryptLib.m
+++ Source/BMExtension/AESCrypt/CryptLib.m
@@ -197,22 +197,6 @@
         return [hash substringToIndex:length];
     }
 }
-- (NSString *)hexadecimalString: (NSData *)data {
-    const unsigned char *dataBuffer = (const unsigned char *)[data bytes];
-
-    if (!dataBuffer) {
-        return [NSString string];
-    }
-
-    NSUInteger dataLength  = [data length];
-    NSMutableString *hexString  = [NSMutableString stringWithCapacity:(dataLength * 2)];
-
-    for (int i = 0; i < dataLength; ++i) {
-        [hexString appendFormat:@"%02x", (unsigned int)dataBuffer[i]];
-    }
-
-    return [NSString stringWithString:hexString];
-}
 
 - (NSString *)hexadecimalString: (NSData *)data {
     const unsigned char *dataBuffer = (const unsigned char *)[data bytes];
