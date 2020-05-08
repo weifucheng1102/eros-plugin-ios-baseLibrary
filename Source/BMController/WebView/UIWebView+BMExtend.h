a5451d231fce6c6f93aa4c6c09a6c892a9b4c37b
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2020-05-08 11:10:34 +0800

    更换WKWebview
    更换WKWebview

--- Source/BMController/WebView/UIWebView+BMExtend.h
+++ Source/BMController/WebView/UIWebView+BMExtend.h
@@ -7,10 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <WebKit/WebKit.h>
 #define K_WebView_FontSize @"K_WebView_FontSize"
 
-@interface UIWebView (BMExtend)
+@interface WKWebView (BMExtend)
 
 /** 根据当前字体缩放级别调整字体 */
 - (void)checkCurrentFontSize;
